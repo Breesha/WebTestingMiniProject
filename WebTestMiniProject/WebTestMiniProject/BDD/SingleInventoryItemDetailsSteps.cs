dcad1200a73125991d24c06bdf4f26e9e318fbe3
bibaxhenis <xbxhenis@gmail.com>
2021-01-20 15:30:28 +0000

    Created POM for the Descriptions Page

---
+++ WebTestMiniProject/WebTestMiniProject/BDD/SingleInventoryItemDetailsSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using WebTestProject;
+
+namespace WebTestMiniProject
+{
+    
+    [Binding]
+    public class SingleInventoryItemDetailsSteps
+    {
+        SD_Website SD_Website = new SD_Website("chrome");
+
+        [Given(@"I am Logged in as a Standard")]
+        public void GivenIAmLoggedInAsAStandard()
+        {
+            SD_Website.SD_SingleItemDescriptionPage.LogIn();
+        }
+        
+        [When(@"I click on the inventory item with the (.*)")]
+        public void WhenIClickOnTheInventoryItemWithThe(string id)
+        {
+            SD_Website.SD_SingleItemDescriptionPage.VisitSingleItemDetailsPage(id);
+        }
+        
+        [Then(@"I can see that items details")]
+        public void ThenICanSeeThatItemsDetails()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ WebTestMiniProject/WebTestMiniProject/lib/pages/SD_SingleItemDescriptionPage.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WebTestProject
+{
+    public class SD_SingleItemDescriptionPage
+    {
+        private IWebDriver _seleniumDriver;
+        private string HomePageURL = AppConfigReader.BaseUrl;
+        private List <IWebElement> _inventoryItems => _seleniumDriver.FindElements(By.ClassName("inventory_item")).ToList();
+
+        internal void LogIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IWebElement _inventoryDetailsBackButton => _seleniumDriver.FindElement(By.ClassName("inventory_details_back_button"));
+
+        private IWebElement _inventoryCartButton => _seleniumDriver.FindElement(By.ClassName("btn_inventory"));
+       
+
+        public SD_SingleItemDescriptionPage(IWebDriver seleniumDriver)
+        {
+            _seleniumDriver = seleniumDriver;
+        }
+
+        public void VisitHomePage()
+        {
+            _seleniumDriver.Navigate().GoToUrl(HomePageURL);
+        }
+
+        public void ClickCartButton()
+        {
+            _inventoryCartButton.Click();
+        }
+
+        public string GetInventoryCartButtnText()
+        {
+            return _inventoryCartButton.Text;
+        }
+        public void VisitSingleItemDetailsPage(string id)
+        {
+            _inventoryItems.Where(item => item.GetAttribute("id") == id).FirstOrDefault().Click();
+        }
+        public void GoToProductsList()
+        {
+            _inventoryDetailsBackButton.Click();
+        }
+        public string GetCurrentPagesURL()
+        {
+            return _seleniumDriver.Url;
+        }
+    }
+}
