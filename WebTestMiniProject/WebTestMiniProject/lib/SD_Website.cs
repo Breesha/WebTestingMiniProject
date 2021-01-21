6603dcf8254492a31d296614a7d28ac074caee4c
Dragonkid1996 <djnewell@btinternet.com>
2021-01-21 11:54:09 +0000

    Class Diagram

--- WebTestMiniProject/WebTestMiniProject/lib/SD_Website.cs
+++ WebTestMiniProject/WebTestMiniProject/lib/SD_Website.cs
@@ -7,14 +7,9 @@ namespace WebTestProject
         public IWebDriver SeleniumDriver { get; internal set; }
         public SD_HomePage SD_HomePage { get; internal set; }
         public SD_Cart SD_Cart { get; internal set; }
-        public SD_CheckoutPage1 SD_CheckoutPage1 { get; internal set; }
-        public SD_CheckoutPage2 SD_CheckoutPage2 { get; internal set; }
         public SD_SingleItemDescriptionPage SD_SingleItemDescriptionPage { get; internal set; }
-
         public SD_YourInfoPage SD_YourInfoPage { get; internal set; }
-
         public SD_LoginPage SD_LoginPage { get; internal set; }
-
 
         public SD_Website(string driverName, int pageLoadWaitInSecs = 10, int implicitWaitInSecs = 10)
         {
@@ -24,18 +19,9 @@ namespace WebTestProject
             //Instantiate page objects with the selenium driver
             SD_HomePage = new SD_HomePage(SeleniumDriver);
             SD_Cart = new SD_Cart(SeleniumDriver);
-            SD_CheckoutPage1 = new SD_CheckoutPage1(SeleniumDriver);
-            SD_CheckoutPage2 = new SD_CheckoutPage2(SeleniumDriver);
-
             SD_SingleItemDescriptionPage = new SD_SingleItemDescriptionPage(SeleniumDriver);
-            
-
-
-
             SD_YourInfoPage = new SD_YourInfoPage(SeleniumDriver);
-
             SD_LoginPage = new SD_LoginPage(SeleniumDriver);
-
         }
 
         public void DeleteCookies()
